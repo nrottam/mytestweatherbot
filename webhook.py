fd5613df58b6c66a642394a53630b0073286e6d8
nrottam <narottam0406@gmail.com>
2018-02-12 16:05:04 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,51 +1,14 @@
-import json
-import os
-import requests
-
-from flask import Flask
-from flask import request
-from flask import make_response
-
-# Flask app should start in global layout
+from flask import Flask, request, Response
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-        req = request.get_json(silent=True, force=True)
-        print(json.dumps(req, indent=4))
+@app.route('/')
+def root_response():
+    return "Hello World."
 
-#        res = makeResponse(req)
-#        res = json.dumps(res, indent=4)
-#        r = make_response(res)
-#         r.headers['Context-Type'] = 'application/json'
-        speech = "The forecast for London is Sunny"
-        return {
-        "speech": speech,
-        "displayText": speech,
-        "source": "apiai-weather-webhook"
-        }
-
-def makeResponse(req):
-        result =req.get("result")
-        parameters = result.get("parameters")
-        city = parameters.get("geo-city")
-        date = parameters.get("date")
-        requests.get('http://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=40c66230d405b5a3797ec5497a397ff1')
-        json_object = r.json
-        for i in range(0,30):
-                if date in weather[i]['dt_txt']:
-                    condition = weather[i]['weather'][0]['description']
-                break
-
-
-        speech = "The forecast for " +city+"for"+date+"is"+condition
-        return {
-        "speech": speech,
-        "displayText": speech,
-        "source": "apiai-weather-webhook"
-        }
+@app.route('/api', methods=['POST', 'GET'])
+def api_response():
+    if request.method == 'POST':
+        return request.json
 
 if __name__ == '__main__':
-        port = int(os.getenv('PORT', 5000))
-        print("Starting app on port %d" % port)
-        app.run(debug=False, port=port, host='0.0.0.0')
+    app.run()
